1c4cce8521849c1202d6d00c79e73864e1b388ab
Shahash Kandel <shahashkandel@gmail.com>
2024-09-14 12:19:50 -0400

    first commit

--- app/src/main/java/com/bignerdranch/android/buttons/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/buttons/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.buttons
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,21 +11,30 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
 
   private  lateinit var myText: TextView
   private lateinit var firstButton: Button
   private lateinit var secondButton: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate() called")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+
+
 
         //initialize by referencing their id
         myText = findViewById(R.id.first_view)
@@ -50,4 +60,36 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart() called")
+    }
+
+
 }
